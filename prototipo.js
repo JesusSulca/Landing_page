c51f3640529089bc509a2d4cc9eeab3a6778de0f
prueba <mail@prueba.com>
2021-10-21 14:43:25 -0500

    Vvrsión 1.1

---
+++ prototipo.js
@@ -0,0 +1,8 @@
+var elemento_tamaño = "70px";
+
+var elemento_inicio = document.getElementById("Inicio");
+elemento_inicio.style.fontSize=elemento_tamaño;
+
+
+var elemento_datos = document.getElementById("datos");
+elemento_datos.href = "#Nombre_form"
